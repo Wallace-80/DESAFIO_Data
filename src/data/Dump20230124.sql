c809f641407e012aa35f0b730ed86ecd45df89f0
Rocio <rocio.ruiz.gonzalez@gmail.com>
2023-01-26 11:46:10 +0100

    actualizacion BBDD

--- src/data/Dump20230124.sql
+++ src/data/Dump20230124.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `travelspain` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `travelspain`;
 USE `travelspain`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
@@ -87,8 +87,8 @@ DROP TABLE IF EXISTS `provincias`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `provincias` (
   `ID_PROVINCIAS` int NOT NULL,
-  `NOMBRE` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `CIUDADES` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL
+  `ENTORNO` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `ACTIVIDADES` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -109,11 +109,9 @@ DROP TABLE IF EXISTS `sitios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `sitios` (
-  `ID_SITIOS` int NOT NULL,
-  `PRECIOS` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `NOMBRE` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `TIPO` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `FK_ID_PROVINCIAS` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL
+  `ID_PROVINCIAS` int NOT NULL,
+  `UBICACION` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `NOMBRE` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -137,10 +135,9 @@ CREATE TABLE `user` (
   `ID_USUARIO` int NOT NULL,
   `EMAIL` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `PASS` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `NOMBRE` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `PROV_VISIT` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `BUSQUEDAS` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
-  `FK_ID_PROVINCIAS` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL
+  `NOMBRE_USUARIO` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `CIUDAD_ORIGEN` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `ID_PROVINCIAS` varchar(160) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -150,7 +147,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'aaa@gmail.com','saldfhsd888x','pedro martin gonzalez','malaga','111','00011cc'),(2,'bbb@hotmail.com','sasdd333tx','Pepe Ruiz Garcia','Sevilla','nosenoseno','0002bb');
+
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
